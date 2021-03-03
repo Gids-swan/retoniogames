093b10cb5a7bacf9b7dfbb71318e349836f54f66
Gids-swan <57260195+Gids-swan@users.noreply.github.com>
2021-03-03 17:03:15 +0000

    Update counter.js
    Added a Counter class that combines the two counter buttons in a class.

--- counter.js
+++ counter.js
@@ -1,15 +1,23 @@
 class CounterButtonUp extends uiWidgets.TextButton {
 	constructor(scene, x, y) {
-		super(scene, x+50, y+40, 'arrowup', function() {console.log("up")}, scene, 0, 0, 0, 0);
+		super(scene, x+50, y+35, 'arrowup', function() {console.log("up")}, scene, 0, 0, 0, 0);
 		this.scene.add.existing(this);
 	}
 }
 
 class CounterButtonDown extends uiWidgets.TextButton {
 	constructor(scene, x, y) {
-		super(scene, x+50, y+40, 'arrowdown', function() {console.log("down")}, scene, 0, 0, 0, 0);
+		super(scene, x+50, y+35, 'arrowdown', function() {console.log("down")}, scene, 0, 0, 0, 0);
 		this.scene.add.existing(this);
 	}
 }
 
-class Counter extends 
+class Counter extends Phaser.GameObjects.Rectangle {
+	constructor(scene, x, y, type) {
+		super(scene, x, y, 120, 170, 0x785419);
+		this.setOrigin(0,0);
+		this.scene.add.existing(this);
+		this.counterup = new CounterButtonUp(scene, x+10, y+10);
+		this.counterdown = new CounterButtonDown(scene, x+10, y+90);
+	}
+}
