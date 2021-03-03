51b8e39d2a431baf3fee24f7c9fe78371139d643
Gids-swan <57260195+Gids-swan@users.noreply.github.com>
2021-03-03 17:07:16 +0000

    Update hotspotscene.js
    Updated the assets for this scene. Generated different colours of flowers. Also adds a counter for testing.

--- hotspotscene.js
+++ hotspotscene.js
@@ -3,10 +3,16 @@ var hotspotScene = new Phaser.Class({
     initialize: function() {
         Phaser.Scene.call(this, { "key": "hotspotScene" });
     },
-    init: function() {},
+    init: function(data) {
+		//console.log( data.hotspotClicked.x );
+	},
     preload: function() {
-		this.load.image('flower', 'assets/yellow-flower-clipart.jpg');
+		this.load.image('yellow', 'assets/yellow-flower-clipart.jpg');
+		this.load.image('blue', 'assets/blue-flower-clipart.jpg');
+		this.load.image('purple', 'assets/purple-flower-clipart.jpg');
 		this.load.image('back', 'assets/blue-button-240.jpg');
+		this.load.image('arrowup', 'assets/arrowup.jpg');
+		this.load.image('arrowdown', 'assets/arrowdown.jpg');
 	},
     create: function() {
        	this.add.rectangle(10, 10, 780, 580, 0x00FF00).setOrigin(0,0);
@@ -18,14 +24,24 @@ var hotspotScene = new Phaser.Class({
 		});
 	
 		let flowers = [];
-		//const length = 7;
 
-		for (i = 0; i < 4; i++) {
+		for (let i = 0; i < 4; i++) {
 			let w = 10 + (Math.random() * 755);
 			let h = 10 + (Math.random() * 555);
 			flowers[i] = new Flower(this, w, h, 'yellow');
 		}
-
+		
+		for (let i = 0; i < 4; i++) {
+			let w = 10 + (Math.random() * 755);
+			let h = 10 + (Math.random() * 555);
+			flowers[i] = new Flower(this, w, h, 'blue');
+		}
+		
+		for (let i = 0; i < 4; i++) {
+			let w = 10 + (Math.random() * 755);
+			let h = 10 + (Math.random() * 555);
+			flowers[i] = new Flower(this, w, h, 'purple');
+		}
 
 		for (let i = 1; i < 16; i++) {
 			let l = this.add.line(i*50, 10, 0, 0, 0, 580, 0x000000, 0.2);
@@ -43,8 +59,6 @@ var hotspotScene = new Phaser.Class({
 	//this.input.setDefaultCursor('assets/zoom.svg, pointer');
 	// .cur files
 	
-	
-	
 		// Temp. cursor
 		zoom = this.add.image(0,0,'zoom');
 		zoom.setScale(0.1);
@@ -53,6 +67,8 @@ var hotspotScene = new Phaser.Class({
 			zoom.x = pointer.x;
 			zoom.y = pointer.y;
 		});
+		
+		let button = new Counter(this, 0, 0, 'flower');
     },
     update: function() {}
 });
