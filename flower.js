16760e25c67b709026bbce7282edf22cc9aafeab
Gids-swan <57260195+Gids-swan@users.noreply.github.com>
2021-03-03 15:29:21 +0000

    Static flower.js methods
    Added a list of flowerTypes and ability to generate a random flower.

--- flower.js
+++ flower.js
@@ -1,11 +1,39 @@
 class Flower extends Phaser.GameObjects.Image {
+	static flowerTypes = ['yellow', 'blue', 'purple'];
+	static generateRandomFlower(scene) {
+		let type = flowerTypes[Math.floor(Math.random()*flowerTypes.length)];
+		let x = 10 + (Math.random() * 755);
+		let y = 10 + (Math.random() * 555);
+		return new Flower(scene, x, y, type);
+	}
 	
-	constructor(scene, x, y)
+	constructor(scene, x, y, type)
 	{
-		super(scene, x, y, 'flower');
+		super(scene, x, y, 'yellow');
+		
+		switch(type) {
+			case 'yellow':
+				this.setTexture('yellow');
+				break;
+				
+			case 'blue':
+				this.setTexture('blue');
+				break;
+			
+			case 'purple':
+				this.setTexture('purple');
+				break;
+			
+			default:
+				this.setTexture('yellow');
+				break;
+		}
+		
 		this.displayWidth = 25;
 		this.displayHeight = 25;
 		this.setOrigin(0,0);
 		this.scene.add.existing(this);
 	}
+	
+	
 }
