c94514ae71349ce6c39303b0e40500bd4da09ff9
Gids-swan <57260195+Gids-swan@users.noreply.github.com>
2021-03-03 17:04:17 +0000

    Update hotspot.js
    Hotspots now store data on the flowers they contain, these are generated randomly. Currently can't use the data to recreate the flowers.

--- hotspot.js
+++ hotspot.js
@@ -1,4 +1,5 @@
 class Hotspot extends Phaser.GameObjects.Ellipse {
+	
 	constructor(scene, x, y)
 	{
 		super(scene, x, y, 20, 20, 0xffd900);
@@ -6,9 +7,15 @@ class Hotspot extends Phaser.GameObjects.Ellipse {
 		// On click do something
 		this.setInteractive().on('pointerdown', function(pointer, localX, localY, event) {
 			this.changeColour();
-			scene.scene.start("hotspotScene");
+			scene.scene.start("hotspotScene", { hotspotClicked : this });
 		});
 		this.scene.add.existing(this);
+		
+		this.flowerData = [];
+		for (let i = 0; i < 10; i++) {
+			this.flowerData.push(FlowerData.generateRandomFlower());
+			console.log(this.flowerData[i].type);
+		}
 	}
 	
 	changeColour() {
