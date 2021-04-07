850a24467416f963fa75a83c91ff416564ea3a09
Gids-swan <57260195+Gids-swan@users.noreply.github.com>
2021-04-08 00:06:18 +0100

    Update randomizer
    Generates several coordinates that can be scrolled through. Each coordinate represents a different quadrat position.
    
    Also made changes to use new UIExtension Tools.

--- global_constants.js
+++ global_constants.js
@@ -14,4 +14,4 @@ const NUM_HOTSPOTS = 7;
 //const MIN_FLOWERS_PER_HOTSPOT = 10;
 const GRID_X_MAX = 16; //inclusive
 const GRID_Y_MAX = 12; //inclusive
-//const QUADRAT_CHECKS_PER_HOTSPOT = 3;
+const QUADRAT_CHECKS_PER_HOTSPOT = 3;
--- randomizer.js
+++ randomizer.js
@@ -5,20 +5,56 @@ class Randomizer extends Phaser.GameObjects.Rectangle {
 		this.setOrigin(0,0);
 		this.scene.add.existing(this);
 		
-		this.countTextX = this.scene.add.text(x+25, y+25, -1, {'fill': '#000', 'font': '16px Courier New'});
-		this.countTextX.setAlign('center');
-		this.countTextX.setOrigin(0.5, 0.5);
+		this.values = [];
+		for (let i = 0; i < QUADRAT_CHECKS_PER_HOTSPOT; i++) {
+			this.values.push(this.generateCoords());
+		}
+		this.currentIndex = 0;
 		
-		this.countTextY = this.scene.add.text(x+75, y+25, -1, {'fill': '#000', 'font': '16px Courier New'});
-		this.countTextY.setAlign('center');
-		this.countTextY.setOrigin(0.5, 0.5);
+		{
+			let me = this;
+			this.leftButton = new RandomizerButtonLeft(scene, 25, me);
+			this.rightButton = new RandomizerButtonRight(scene, 25, me);
+		}
 		
-		this.setInteractive().on('pointerdown', function(pointer, localX, localY, event){
-			let valueX = Math.floor(Math.random() * ((GRID_X_MAX+1) - 0)) + 0;
-			this.countTextX.setText(valueX);
-			
-			let valueY = Math.floor(Math.random() * ((GRID_Y_MAX+1) - 0)) + 0;
-			this.countTextY.setText(valueY);
-		});
+		this.countTextX = this.scene.add.centredtextex(x+25, y+25, this.values[0][0], {'fill': '#000', 'font': '16px Courier New'});
+		this.countTextY = this.scene.add.centredtextex(x+75, y+25, this.values[0][1], {'fill': '#000', 'font': '16px Courier New'});
+	}
+	
+	generateCoords() {
+		let coords = [-1, -1];
+		coords[0] = Math.floor(Math.random() * ((GRID_X_MAX+1) - 0)) + 0;
+		coords[1] = Math.floor(Math.random() * ((GRID_Y_MAX+1) - 0)) + 0;
+		return coords;
+	}
+	
+	next() {
+		if (this.currentIndex + 1 < this.values.length) {
+			this.currentIndex++;
+			this.countTextX.setText(this.values[this.currentIndex][0]);
+			this.countTextY.setText(this.values[this.currentIndex][1]);
+		}
+	}
+	
+	previous() {
+		if (this.currentIndex - 1 >= 0) {
+			this.currentIndex--;
+			this.countTextX.setText(this.values[this.currentIndex][0]);
+			this.countTextY.setText(this.values[this.currentIndex][1]);
+		}
+	}
+}
+
+class RandomizerButtonLeft extends TextButtonEX {
+	constructor(scene, width, randomizer) {
+		super(scene, randomizer.x - width, randomizer.y, 'arrowleft', function() {randomizer.previous(); scene.sound.play('click');}, scene);
+		this.setDimensions(width, randomizer.height);
+	}
+}
+
+class RandomizerButtonRight extends TextButtonEX {
+	constructor(scene, width, randomizer) {
+		super(scene, randomizer.x + randomizer.width, randomizer.y, 'arrowright', function() {randomizer.next(); scene.sound.play('click');}, scene);
+		this.setDimensions(width, randomizer.height);
 	}
 }
